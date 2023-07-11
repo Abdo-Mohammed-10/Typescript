93e31c12b0241451627fec8bbaa1e46627bb47f3
Abdulrahman Mohammed <125462586+Abdo-Mohammed-10@users.noreply.github.com>
2023-07-11 21:27:33 +0300

    Annnotation Arrays

---
+++ Annnotation With Arrays/index.ts
@@ -0,0 +1,18 @@
+// Type Annotation with array 
+var all = "abdo";
+all = "a";
+all = 20;
+all = true;
+var mybest = ["mohammed", "abdo"];
+for (var i = 0; i < mybest.length; i++) {
+    console.log(mybest[i].repeat(5));
+}// Type Annotation with array 
+var all = "abdo";
+all = "a";
+all = 20;
+all = true;
+var mybest = ["mohammed", "abdo"];
+for (var i = 0; i < mybest.length; i++) {
+    console.log(mybest[i].repeat(5));
+}
+
