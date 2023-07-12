b997e736e3727a27772b8570879144b84e4d8bb7
Abdulrahman Mohammed <125462586+Abdo-Mohammed-10@users.noreply.github.com>
2023-07-12 08:36:12 +0300

    lecture 1

---
+++ Type-Anootation And Any Data  Type/tesr.js
@@ -0,0 +1,5 @@
+function add (n1,n2){
+    return n1+n2;
+}
+console.log(add(10,10));
+console.log(typeof add(10,15));
---
+++ Type-Anootation And Any Data  Type/test.ts
@@ -0,0 +1,12 @@
+let thename ="abdulrahman";
+let age : number=20;
+let hire : boolean= true;
+let all:any="abdo";//Any
+let allVars;
+thename="abdo";
+all = 100;
+function add (n1:number,n2:number){
+    return n1+n2;
+}
+console.log(add(10,10));
+console.log(typeof add(10,15));
