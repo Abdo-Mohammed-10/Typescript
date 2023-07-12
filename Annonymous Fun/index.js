deaeca950bba23ffd9663621a5381f27730b298e
Abdulrahman Mohammed <125462586+Abdo-Mohammed-10@users.noreply.github.com>
2023-07-13 00:51:13 +0300

    Annoynmous Fun with Arrow Array

---
+++ Annonymous Fun/index.js
@@ -0,0 +1,8 @@
+//Annonymous Fun
+//Arrow Fun
+var add = function (num1, num2) {
+    return num1 + num2;
+};
+console.log(add(10, 30));
+var addWitharrow = function (num1, num2) { return num1 + num2; };
+console.log(addWitharrow(50, 30));
---
+++ Annonymous Fun/index.ts
@@ -0,0 +1,10 @@
+//Annonymous Fun
+//Arrow Fun
+const add=function(num1:number,num2:number):number{
+    return num1+num2;
+}
+console.log(add(10,30));
+
+
+const addWitharrow = (num1:number ,num2:number):number=> num1+num2;
+console.log(addWitharrow(10,30));
