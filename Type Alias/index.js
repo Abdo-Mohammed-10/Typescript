892715ea27727b24324340dc489a5eae8865edf1
Abdulrahman Mohammed <125462586+Abdo-Mohammed-10@users.noreply.github.com>
2023-07-13 10:35:18 +0300

    Alias

---
+++ Type Alias/index.js
@@ -0,0 +1,7 @@
+var thename = "abdo";
+thename = "mohammed";
+console.log(thename);
+var all = 10;
+all = 100;
+all = "abdulrahman";
+console.log(all);
---
+++ Type Alias/index.ts
@@ -0,0 +1,9 @@
+type st =string;
+let thename : st ="abdo";
+thename="mohammed";
+console.log(thename)
+type standnum = string|number;
+let all : standnum=10;
+all =100;
+all ="abdulrahman"
+console.log(all)
