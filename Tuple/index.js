162f9142d3bd85a70d153986e0ed98b02aa673c4
Abdulrahman Mohammed <125462586+Abdo-Mohammed-10@users.noreply.github.com>
2023-07-13 10:37:36 +0300

    Tuple

---
+++ Tuple/index.js
@@ -0,0 +1,10 @@
+//Tuple
+var article = ["abdo", 5, true];
+article = ["mohammed", 2, false];
+article.push(66);
+article.push("abdulrahman");
+console.log(article);
+var id = article[0], counter = article[1], arepublished = article[2];
+console.log(id);
+console.log(counter);
+console.log(arepublished);
---
+++ Tuple/index.ts
@@ -0,0 +1,11 @@
+//Tuple
+let article:[string,number,boolean]=["abdo",5,true];
+article=["mohammed",2,false];
+article.push(66);
+article.push("abdulrahman");
+
+console.log(article);
+const[id,counter,arepublished]=article;
+console.log(id); 
+console.log(counter);
+console.log(arepublished);
