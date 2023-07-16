8d70cabeef7bf7430732318420cd208e92992464
Abdulrahman Mohammed <125462586+Abdo-Mohammed-10@users.noreply.github.com>
2023-07-17 01:37:31 +0300

    Assertions

---
+++ Assertions/index.js
@@ -0,0 +1,5 @@
+//let myimg=document.getElementById("my-img") as HTMLImageElement;
+// let myimg=<HTMLImageElement>document.getElementById("my-img") ;
+//console.log(myimg.src);
+var data = "Abdo";
+console.log(data.repeat(3));
---
+++ Assertions/index.ts
@@ -0,0 +1,7 @@
+//let myimg=document.getElementById("my-img") as HTMLImageElement;
+// let myimg=<HTMLImageElement>document.getElementById("my-img") ;
+//console.log(myimg.src);
+
+let data :any = "Abdo" ;
+console.log((data as string).repeat(3));
+
